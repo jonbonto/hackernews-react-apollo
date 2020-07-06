3f4e506db5f21d46104d969561a168e3aa2a34f4
Khalimudin <khalimudin@gmail.com>
2020-07-06 21:18:55 +0700

    pagination

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -3,6 +3,7 @@ import gql from "graphql-tag"
 import { Mutation } from 'react-apollo'
 
 import { FEED_QUERY } from "./LinkList"
+import { LINKS_PER_PAGE } from '../constants'
 
 const POST_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
@@ -25,14 +26,14 @@ export class CreateLink extends Component {
     return (
       <div>
         <div className="flex flex-column mt3">
-          <input 
+          <input
             className="mb2"
             value={description}
             onChange={e => this.setState({ description: e.target.value })}
             type="text"
             placeholder="A description for the link"
           />
-          <input 
+          <input
             className="mb2"
             value={url}
             onChange={e => this.setState({ url: e.target.value })}
@@ -40,16 +41,23 @@ export class CreateLink extends Component {
             placeholder="The URL for the link"
           />
         </div>
-        <Mutation 
-          mutation={POST_MUTATION} 
+        <Mutation
+          mutation={POST_MUTATION}
           variables={{ description, url }}
-          onCompleted={() => this.props.history.push('/')}
+          onCompleted={() => this.props.history.push('/new/1')}
           update={(store, { data: { post } }) => {
-            const data = store.readQuery({ query: FEED_QUERY })
+            const first = LINKS_PER_PAGE
+            const skip = 0
+            const orderBy = 'createdAt_DESC'
+            const data = store.readQuery({
+              query: FEED_QUERY,
+              variables: { first, skip, orderBy }
+            })
             data.feed.links.unshift(post)
             store.writeQuery({
               query: FEED_QUERY,
-              data
+              data,
+              variables: { first, skip, orderBy }
             })
           }}
         >
